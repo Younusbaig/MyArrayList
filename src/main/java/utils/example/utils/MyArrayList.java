2420105e2bfba906b45bd6ea3b94376714b4732b
MuhammadYounusBaig <myounusbaig93@gmail.com>
2022-01-28 16:28:10 +0500

    CustomArrayList with Test Case

--- src/main/java/utils/example/utils/MyArrayList.java
+++ src/main/java/utils/example/utils/MyArrayList.java
@@ -1,67 +1,26 @@
 package utils.example.utils;
 
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class MyArrayList {
 
-    User user1 = new User(1, "Ali", "karachi");
-    User user2 = new User(2, "Muhammad", "Lahore");
-    User user3 = new User(3, "Abdullah", "Lahore");
-    User user4 = new User(4, "Rehan", "Peshawar");
-    User user5 = new User(5, "Owais", "Quetta");
+    private String[] array = new String[10];
+    private int size = 0;
 
-    List<User> list = new ArrayList<>();
 
-    public void addList() {
-        list.add(user1);
-        list.add(user2);
-        list.add(user3);
-        list.add(user4);
-        list.add(user5);
-    }
-
-    public void getList() {
-        for (int i=0; i<list.size(); i++){
-            list.get(i);
+    public void add(String string) {
+        array[size] = string;
+        size++;
+        if (size == array.length) {
+            String[] newArray = new String[array.length * 2];
+            for (int i = 0; i < array.length; i++) {
+                newArray[i] = array[i];
+            }
+            array = newArray;
         }
-
     }
+
+    public String get(int index) {
+        return array[index];
+    }
+
 }
-
-    class User {
-        private Integer id;
-        private String name;
-        private String city;
-
-        public User(Integer id, String name, String city) {
-            this.city = city;
-            this.name = name;
-            this.id = id;
-        }
-
-        public int getId() {
-            return id;
-        }
-
-        public void setId(int id) {
-            this.id = id;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getCity() {
-            return city;
-        }
-
-        public void setCity(String city) {
-            this.city = city;
-        }
-    }
--- src/test/java/utils/example/utils/MyArrayListTest.java
+++ src/test/java/utils/example/utils/MyArrayListTest.java
@@ -1,26 +1,31 @@
 package utils.example.utils;
 
 
-
 import org.junit.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MyArrayListTest {
 
     @Test
-     public void addListTest(){
-        User user = new User(1, "Muhammad", "Karachi");
-        assertEquals(user.getId(), 1, "id");
-        assertEquals(user.getName(), "Muhammad", "name");
-        assertEquals(user.getCity(), "Karachi", "city");
+    public void addListTest() {
+        MyArrayList expectedEmployeeData = new MyArrayList();
+        expectedEmployeeData.add("Muhammad");
+        expectedEmployeeData.add("Ahmed");
+        expectedEmployeeData.add("abdullah");
+
+        MyArrayList employee = new MyArrayList();
+        assertNotNull(expectedEmployeeData);
+        assertEquals(employee.equals(expectedEmployeeData), expectedEmployeeData.equals(employee));
     }
 
     @Test
-    public void addListTestNotEqual(){
-        User user = new User(2,"Ahmed", "Lahore");
-        assertNotEquals(user.getCity(), null, "city");
-        assertNotEquals(user.getId(), "1", "id");
-        assertNotEquals(user.getName(), null, "name");
+    public void getIndexTest(){
+        MyArrayList employee = new MyArrayList();
+        employee.add("Muahammad");
+        employee.add("Rehan");
+        employee.add("Faizan");
+        employee.get(0);
+        assertNotNull(employee);
     }
 }
